002537dd66b2976588ec212b81e649ec89d24d44
dakauann <dakauannc@gamil.com>
2021-12-25 19:33:14 -0300

    Syncing versions, npm > github

--- speech.js
+++ speech.js
@@ -8,50 +8,109 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
 exports.getAudioUrl = void 0;
-const request = require('request');
-const fetch = require('node-fetch');
+var request = require('request');
+var fetch = require('node-fetch');
 function wait() {
-    return new Promise(resolve => resolve(20));
+    return new Promise(function (resolve) { return resolve(20); });
 }
 function getAudioUrl(key, secretKey, carachter, text) {
+    var _this = this;
     if (carachter === undefined)
         throw new Error('Define the carachter voice.');
     if (key === undefined)
         throw new Error('Define the key you got from uberduck');
     if (carachter === undefined)
         throw new Error('Define the secret key u got from uberduck.');
-    return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-        yield request({
-            url: 'https://api.uberduck.ai/speak',
-            method: 'POST',
-            body: `{"speech": "${text}","voice": "${carachter}"}`,
-            auth: {
-                'user': key,
-                'pass': secretKey
+    return new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+        var _this = this;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, request({
+                        url: 'https://api.uberduck.ai/speak',
+                        method: 'POST',
+                        body: "{\"speech\": \"".concat(text, "\",\"voice\": \"").concat(carachter, "\"}"),
+                        auth: {
+                            'user': key,
+                            'pass': secretKey
+                        }
+                    }, function (erro, response, body) { return __awaiter(_this, void 0, void 0, function () {
+                        function getJson(url) {
+                            return __awaiter(this, void 0, void 0, function () {
+                                var jsonResult;
+                                return __generator(this, function (_a) {
+                                    switch (_a.label) {
+                                        case 0:
+                                            jsonResult = undefined;
+                                            return [4 /*yield*/, fetch(url)
+                                                    .then(function (res) { return res.json(); })
+                                                    .then(function (json) {
+                                                    jsonResult = json;
+                                                })];
+                                        case 1:
+                                            _a.sent();
+                                            return [2 /*return*/, jsonResult];
+                                    }
+                                });
+                            });
+                        }
+                        var audioResponse, jsonResponse;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0:
+                                    if (erro)
+                                        throw new Error('Error when making request, verify if yours params (key, secretKey, carachter) are correct.');
+                                    audioResponse = 'https://api.uberduck.ai/speak-status?uuid=' + JSON.parse(body).uuid;
+                                    jsonResponse = false;
+                                    return [4 /*yield*/, getJson(audioResponse)];
+                                case 1:
+                                    jsonResponse = _a.sent();
+                                    _a.label = 2;
+                                case 2:
+                                    if (!(jsonResponse.path === null)) return [3 /*break*/, 4];
+                                    return [4 /*yield*/, getJson(audioResponse)];
+                                case 3:
+                                    jsonResponse = _a.sent();
+                                    return [3 /*break*/, 2];
+                                case 4:
+                                    resolve(jsonResponse.path);
+                                    return [2 /*return*/];
+                            }
+                        });
+                    }); })];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
             }
-        }, (erro, response, body) => __awaiter(this, void 0, void 0, function* () {
-            if (erro)
-                throw new Error('Error when making request, verify if yours params (key, secretKey, carachter) are correct.');
-            const audioResponse = 'https://api.uberduck.ai/speak-status?uuid=' + JSON.parse(body).uuid;
-            let jsonResponse = false;
-            function getJson(url) {
-                return __awaiter(this, void 0, void 0, function* () {
-                    let jsonResult = undefined;
-                    yield fetch(url)
-                        .then(res => res.json())
-                        .then(json => {
-                        jsonResult = json;
-                    });
-                    return jsonResult;
-                });
-            }
-            jsonResponse = yield getJson(audioResponse);
-            while (jsonResponse.path === null)
-                jsonResponse = yield getJson(audioResponse);
-            resolve(jsonResponse.path);
-        }));
-    }));
+        });
+    }); });
 }
 exports.getAudioUrl = getAudioUrl;
